dbcb9343b142f5a0a8e8302eade9eee693d25785
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2023-12-21 11:44:24 +0530

    designing part is done

--- labor/views.py
+++ labor/views.py
@@ -5,8 +5,28 @@ from django.shortcuts import render, HttpResponse
 def login_view(request):
     return render(request, 'login.html')
 
+def register_request_view(request):
+    return render(request, 'register-request.html')
+
+def forgot_password_view(request):
+    return render(request, 'forgot-password.html')
+
+def otp_verify_view(request):
+    return render(request, 'otp-verification.html')
+
+
+
 def dashboard_view(request):
     return render(request, 'dashboard.html')
 
 def tasks_view(request):
     return render(request, 'tasks.html')
+
+def parties_view(request):
+    return render(request, 'parties.html')
+
+def payments_view(request):
+    return render(request, 'payments.html')
+
+def profile_view(request):
+    return render(request, 'profile.html')
